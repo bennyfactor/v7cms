a0b0bff1a1a11980b4877d4bdeb3d864428e2f3e
Benjamin Lamb <bennyfactor@iaatb.net>
2026-03-18 14:23:41 -0400

    Fix counter cache migration to use raw SQL instead of reset_counters
    The migration-local Menu model's has_many :menu_items association
    caused reset_counters to fail in CI looking for MenuItem class in
    the migration namespace. Replace with a simple SQL UPDATE subquery.

--- db/migrate/20260318000002_add_menu_items_count_to_menus.rb
+++ db/migrate/20260318000002_add_menu_items_count_to_menus.rb
@@ -1,19 +1,16 @@
 # frozen_string_literal: true
 
 class AddMenuItemsCountToMenus < ActiveRecord::Migration[7.0]
-  class Menu < ActiveRecord::Base
-    self.table_name = 'menus'
-    has_many :menu_items, foreign_key: :menu_id
-  end
-
   def change
     add_column :menus, :menu_items_count, :integer, default: 0, null: false
 
     reversible do |dir|
       dir.up do
-        Menu.find_each do |menu|
-          Menu.reset_counters(menu.id, :menu_items)
-        end
+        execute <<-SQL.squish
+          UPDATE menus SET menu_items_count = (
+            SELECT COUNT(*) FROM menu_items WHERE menu_items.menu_id = menus.id
+          )
+        SQL
       end
     end
   end
