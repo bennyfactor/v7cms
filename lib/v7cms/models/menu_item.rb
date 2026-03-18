d032acbd7f228ad98568106fb9b90c930c75147c
Benjamin Lamb <bennyfactor@iaatb.net>
2026-03-17 22:58:45 -0400

    Add MenuItem model with polymorphic links, depth enforcement, and href generation

---
+++ lib/v7cms/models/menu_item.rb
@@ -0,0 +1,69 @@
+# frozen_string_literal: true
+
+module V7CMS
+  class MenuItem < ActiveRecord::Base
+    belongs_to :menu
+    belongs_to :parent, class_name: 'V7CMS::MenuItem', optional: true
+    has_many :children, -> { order(:position) }, class_name: 'V7CMS::MenuItem',
+             foreign_key: :parent_id, dependent: :destroy
+
+    belongs_to :linkable, polymorphic: true, optional: true
+
+    LINK_TYPES = %w[page post custom].freeze
+
+    validates :label, presence: true
+    validates :link_type, inclusion: { in: LINK_TYPES }
+    validates :url, presence: true, if: -> { link_type == 'custom' }
+    validates :linkable, presence: true, if: -> { link_type.in?(%w[page post]) }
+    validate :prevent_circular_reference
+    validate :enforce_max_depth
+
+    after_commit :trigger_static_regeneration
+
+    def href
+      case link_type
+      when 'page'
+        linkable ? "/#{linkable.full_slug_path}" : '#'
+      when 'post'
+        linkable ? "/posts/#{linkable.slug}" : '#'
+      when 'custom'
+        url
+      else
+        '#'
+      end
+    end
+
+    def to_nested_hash
+      {
+        id: id,
+        label: label,
+        href: href,
+        target: target,
+        children: children.includes(:linkable).map(&:to_nested_hash)
+      }
+    end
+
+    private
+
+    def prevent_circular_reference
+      return if parent_id.nil?
+
+      if parent_id == id
+        errors.add(:parent_id, 'cannot be self')
+      end
+    end
+
+    def enforce_max_depth
+      return if parent_id.nil?
+      return unless parent
+
+      if parent.parent_id.present?
+        errors.add(:parent_id, 'maximum nesting depth is 2 levels')
+      end
+    end
+
+    def trigger_static_regeneration
+      menu&.regenerate_all_static_files
+    end
+  end
+end
