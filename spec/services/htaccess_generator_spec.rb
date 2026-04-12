bede753bae5135d199900f41e1c009647d631dd8
Benjamin Lamb <bennyfactor@iaatb.net>
2026-04-11 20:29:50 -0400

    Fix specs missed in directory structure change
    Update post model spec and htaccess generator spec to use the new
    slug/index.html path pattern.

--- spec/services/htaccess_generator_spec.rb
+++ spec/services/htaccess_generator_spec.rb
@@ -63,9 +63,8 @@ RSpec.describe HtaccessGenerator do
     end
 
     it 'includes rewrite rules for pre-generated HTML directories' do
-      %w[posts pages].each do |dir|
-        expect(template).to include("RewriteRule ^#{dir}/(.+\\.html)$ /public/#{dir}/$1 [L]")
-      end
+      expect(template).to include('RewriteRule ^posts/([^/.]+)/?$ /public/posts/$1/index.html [L]')
+      expect(template).to include('RewriteRule ^pages/(.+?)/?$ /public/pages/$1/index.html [L]')
     end
 
     it 'sets long cache for static asset file types' do
